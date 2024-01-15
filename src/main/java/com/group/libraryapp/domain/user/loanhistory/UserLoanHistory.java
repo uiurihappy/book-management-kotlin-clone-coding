c75bb248c906794ae561ef1bc1b033e61f22dc29
ybchar <uiurihappy@sju.ac.kr>
2024-01-15 18:05:50 +0900

    project init

---
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -0,0 +1,44 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import com.group.libraryapp.domain.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class UserLoanHistory {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	private User user;
+
+	private String bookName;
+
+	private boolean isReturn;
+
+	public UserLoanHistory() {
+
+	}
+
+	public UserLoanHistory(User user, String bookName, boolean isReturn) {
+		this.user = user;
+		this.bookName = bookName;
+		this.isReturn = isReturn;
+	}
+
+	public String getBookName() {
+		return this.bookName;
+	}
+
+	public void doReturn() {
+		this.isReturn = true;
+	}
+
+}
